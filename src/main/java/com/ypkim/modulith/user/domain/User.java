0a8684a2dac5cb7d46588a53da619e57e40c2d13
ypkim <ypkim0923@gmail.com>
2025-02-03 23:11:25 +0900

    User Repository 추가

--- src/main/java/com/ypkim/modulith/user/domain/User.java
+++ src/main/java/com/ypkim/modulith/user/domain/User.java
@@ -1,11 +1,31 @@
 package com.ypkim.modulith.user.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String phone;
 }
--- src/main/java/com/ypkim/modulith/user/domain/UserService.java
+++ src/main/java/com/ypkim/modulith/user/domain/UserService.java
@@ -1,11 +1,17 @@
 package com.ypkim.modulith.user.domain;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository repository;
+
     public User save(User user) {
-        return user;
+        return repository.save(user);
     }
 }
