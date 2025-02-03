b78e3dcc93db7913c3ba2cf967a4eadadb734b51
ypkim <ypkim0923@gmail.com>
2025-02-03 21:20:31 +0900

    어플리케이션 테스트 추가

--- src/test/java/com/ypkim/modulith/ModulithApplicationTests.java
+++ src/test/java/com/ypkim/modulith/ModulithApplicationTests.java
@@ -2,12 +2,21 @@ package com.ypkim.modulith;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
+import org.springframework.modulith.test.ApplicationModuleTest;
 
-@SpringBootTest
+@ApplicationModuleTest
 class ModulithApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void moduleTest() {
+		ApplicationModules modules = ApplicationModules.of(ModulithApplication.class).verify();
+		modules.forEach(System.out::println);
+
+		new Documenter(modules)
+			.writeDocumentation()
+			.writeIndividualModulesAsPlantUml();
 	}
 
 }
