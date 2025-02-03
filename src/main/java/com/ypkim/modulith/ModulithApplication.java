642a30f6adfe3e883918b96ca66f16b709bc589f
ypkim <ypkim0923@gmail.com>
2025-02-03 20:16:54 +0900

    SpringApplication에 Modulith 애노테이션을 추가하여 Modulith 어플리케이션으로 동작하도록 추가

--- src/main/java/com/ypkim/modulith/ModulithApplication.java
+++ src/main/java/com/ypkim/modulith/ModulithApplication.java
@@ -2,8 +2,10 @@ package com.ypkim.modulith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulith;
+import org.springframework.modulith.Modulithic;
 
-@SpringBootApplication
+@Modulith
 public class ModulithApplication {
 
 	public static void main(String[] args) {
